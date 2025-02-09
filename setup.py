0e18f58c489d0fd36ae4ac8b4fa9d58c171ad393
gopalakrishnanarjun <gopalakrishnana02@gmail.com>
2025-02-09 21:57:01 +0530

    uploading the code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydatalens",
-    version="0.0.5",
+    version="0.0.6",
     description="A Python package for automatic EDA, data cleaning, and visualization.",
     author='Gopalakrishn Arjunan',
     author_email='gopalakrishnana02@gmail.com',
